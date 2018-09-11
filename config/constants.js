844ecc8434a0cb618ef75d1cb19722f97b18058b
WK <formyjihad@gmail.com>
2018-09-11 17:58:52 +0900

    20180911 main commit

--- config/constants.js
+++
@@ -1,3 +0,0 @@
-export const IMP_KEY = '6265854440697143'
-export const IMP_SECRET = 'kOlyqI840OAOk2fO7SMP2gcaqGy50clk0GTrp1wpQ6db9tZhlL7n2jytZ6y0jTkkAeLZghp6oChx5hPr'
-export const IMP_CODE = 'imp11436362'
