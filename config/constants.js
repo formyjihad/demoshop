a82df42b5ab72ffb08fb40ce47d9b6c141c0882e
nakunjoo <nkj960822@gmail.com>
2019-03-22 17:52:23 +0900

    temp commit

---
+++ config/constants.js
@@ -0,0 +1,7 @@
+export const IMP_KEY = '6265854440697143'
+export const IMP_SECRET = 'kOlyqI840OAOk2fO7SMP2gcaqGy50clk0GTrp1wpQ6db9tZhlL7n2jytZ6y0jTkkAeLZghp6oChx5hPr'
+export const IMP_CODE = 'imp11436362'
+export const SS_ID = "825579814898-oec301je5gvtbp9hhhg730k7ead5fl3m.apps.googleusercontent.com"
+export const SS_KEY = "IxHKDMLpPfq96IEgCdeLF173"
+export const SS_URL = "http://localhost/api/users/auth/google/callback"
+export const JUSO_KEY = "U01TX0FVVEgyMDE4MDkwNDExNDgxNzEwODExMTI="
---
+++ config/jwt.js
@@ -0,0 +1,1 @@
+export const Secret_key =  "#123#123#asd#qwe#zxc#qwer#128*(*&asdjk"
